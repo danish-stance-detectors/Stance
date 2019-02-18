0391d56fe3ff9490f57cee0afca1e896c93de174
anders <anders@edelbolillie.dk>
2019-02-18 15:01:19 +0100

    Load FastText word embeddings

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,9 +1,7 @@
 import os
-from gensim.test.utils import datapath
+from gensim.models.fasttext import FastText
 
 datafolder = '../data/'
-fasttext_data = os.path.join(datafolder, 'cc.da.300.bin')
+fasttext_data = os.path.join(datafolder, 'fasttext/cc.da.300.bin')
 
-cap_path = datapath(fasttext_data)
-fb_partial = FastText.load_fasttext_format(cap_path, encoding='utf-8', full_model=False)
-'æble' in fb_partial.wv.vocab
+fb = FastText.load_fasttext_format(fasttext_data, encoding='utf8')
