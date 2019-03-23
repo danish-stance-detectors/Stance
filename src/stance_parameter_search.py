8f549848d179ae2d017faf71f273f88c6d3bbb63
anders <anders@edelbolillie.dk>
2019-03-23 18:12:52 +0100

    Basic Grid parameter search

---
+++ src/stance_parameter_search.py
@@ -0,0 +1,65 @@
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+import data_loader
+
+training_data = '../data/preprocessed/preprocessed.csv'
+instances, _ = data_loader.get_instances(training_data, '\t')
+
+X = [x[2] for x in instances]
+y = [x[1] for x in instances]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.25, random_state=0
+)
+
+settings = [
+    ('rbf-svm', SVC(), {'kernel': ['rbf'], 'gamma': [1e-3, 1e-4], 'C': [1, 10, 100, 1000]}),
+    ('linear-svm', SVC(), {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}),
+    ('tree', DecisionTreeClassifier(), {'criterion': ['entropy', 'gini'], 'splitter':['best', 'random']}),
+    ('logistic-regression', LogisticRegression(), {'solver': ['liblinear'], 'penalty':['l1', 'l2'],
+                                                   'C': [1, 10, 100, 1000], 'multi_class': ['auto']}),
+    ('random-forest', RandomForestClassifier(), {'n_estimators': [10, 20, 50, 100], 'criterion': ['entropy', 'gini']})
+
+]
+
+scores = [
+    'accuracy',
+    'f1_macro'
+]
+
+for score in scores:
+    for name, estimator, tuned_parameters in settings:
+        print("# Tuning hyper-parameters for %s with %s" % (name, score))
+        print()
+        clf = GridSearchCV(
+            estimator, tuned_parameters, cv=5, scoring=score, n_jobs=-1, error_score=0
+        )
+        clf.fit(X_train, y_train)
+
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r"
+                  % (mean, std * 2, params))
+        print()
+
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(X_test)
+        print(classification_report(y_true, y_pred))
+        print()
+
