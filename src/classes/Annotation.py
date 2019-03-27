f23f17749caacd88884f3872c40d1b78c5ed75b6
anders <anders@edelbolillie.dk>
2019-03-27 16:58:57 +0100

    Adapt to branches and source

--- src/classes/Annotation.py
+++ src/classes/Annotation.py
@@ -1,11 +1,14 @@
 from nltk import word_tokenize
 import re
 
+url_tag = 'URLURLURL'
+quote_tag = 'REFREFREF'
 
-class CommentAnnotation:
+class RedditAnnotation:
         
     # initialises comment annotation class given json
-    def __init__(self, json, test=False):
+    def __init__(self, json, is_source=False, test=False):
+        self.is_source = is_source
 
         if test:
             self.comment_id = "test"
@@ -15,45 +18,91 @@ class CommentAnnotation:
             # sdcq is just placeholder values
             self.sdqc_parent = "Supporting"
             self.sdqc_submission = "Supporting"
+            return
+
+        comment_json = json["comment"] if not is_source else json
+        self.text = comment_json["text"]
+        self.text = self.filter_reddit_quotes(self.text)
+        self.text = self.filter_text_urls(self.text)
+        if is_source:
+            self.title = json["title"]
+            self.num_comments = json["num_comments"]
+            self.url = json["url"]
+            self.text_url = json["text_url"]
+            self.is_video = json["is_video"]
+            # self.subreddit = json["subreddit"] # irrelevant
+            # self.comments = json["comments"] # irrelevant
+            self.is_rumour = json["IsRumour"]
+            self.is_irrelevant = json["IsIrrelevant"]
+            self.truth_status = json["TruthStatus"]
+            self.rumour = json["RumourDescription"]
+            self.sdqc_source = json["SourceSDQC"]
+            self.tokens = self.tokenize(self.title)
         else:
-            # comment info
-            self.submission_id = json["comment"]["SubmissionID"]
-            self.comment_id = json["comment"]["comment_id"]
-            self.text = json["comment"]["text"]
-            self.parent_id = json["comment"]["parent_id"]
-            self.comment_url = json["comment"]["comment_url"]
-            self.created = json["comment"]["created"]
-            self.upvotes = json["comment"]["upvotes"]
-            self.is_submitter = json["comment"]["is_submitter"]
-            self.is_deleted = json["comment"]["is_deleted"]
-            self.reply_count = json["comment"]["replies"]
-            
-            # user info
-            self.user_id = json["comment"]["user"]["id"]
-            self.user_name = json["comment"]["user"]["username"]
-            self.user_created = json["comment"]["user"]["created"]
-            self.user_karma = json["comment"]["user"]["karma"]
-            self.user_gold_status = json["comment"]["user"]["gold_status"]
-            self.user_is_employee = json["comment"]["user"]["is_employee"]
-            self.user_has_verified_email = json["comment"]["user"]["has_verified_email"]
+            # comment specific info
+            self.comment_id = comment_json["comment_id"]
+            self.parent_id = comment_json["parent_id"]
+            self.comment_url = comment_json["comment_url"]
+            self.is_submitter = comment_json["is_submitter"]
+            self.is_deleted = comment_json["is_deleted"]
+            self.reply_count = comment_json["replies"]
+            self.tokens = self.tokenize(comment_json["text"])
 
             # annotation info
             self.annotator = json["annotator"]
-            self.sdqc_parent = json["comment"]["SDQC_Parent"]
-            self.sdqc_submission = json["comment"]["SDQC_Submission"]
-            self.certainty = json["comment"]["Certainty"]
-            self.evidentiality = json["comment"]["Evidentiality"]
-            self.annotated_at = json["comment"]["AnnotatedAt"]
+            self.sdqc_parent = comment_json["SDQC_Parent"]
+            self.sdqc_submission = comment_json["SDQC_Submission"]
+            self.certainty = comment_json["Certainty"]
+            self.evidentiality = comment_json["Evidentiality"]
+            self.annotated_at = comment_json["AnnotatedAt"]
 
-            # Remove non-alphabetic characters and tokenize
-            text_ = re.sub("[^a-zA-ZæøåÆØÅ]", " ", self.text) # replace with space
-            # Convert all words to lower case and tokenize
-            self.tokens = word_tokenize(text_.lower())
+        # general info
+        self.submission_id = comment_json["submission_id"]
+        self.created = comment_json["created"]
+        self.upvotes = comment_json["upvotes"]
 
-class Annotations:
+        # user info
+        self.user_id = comment_json["user"]["id"]
+        self.user_name = comment_json["user"]["username"]
+        self.user_created = comment_json["user"]["created"]
+        self.user_karma = comment_json["user"]["karma"]
+        self.user_gold_status = comment_json["user"]["gold_status"]
+        self.user_is_employee = comment_json["user"]["is_employee"]
+        self.user_has_verified_email = comment_json["user"]["has_verified_email"]
+
+    def tokenize(self, text):
+        # Remove non-alphabetic characters and tokenize
+        text_ = re.sub("[^a-zA-ZæøåÆØÅ0-9]", " ", text)  # replace with space
+        # Convert all words to lower case and tokenize
+        return word_tokenize(text_.lower())
+
+    def filter_reddit_quotes(self, text):
+        """filters text of all annotations to replace reddit quotes with 'REFREFREF'"""
+        # TODO: Doesn't seem to replace quotes? Also, do we want it? Also quotes are sometimes " "
+        return re.sub(r"^>*\n$", quote_tag, text)
+
+    def filter_text_urls(self, text):
+        """filters text of all annotations to replace 'URLURLURL'"""
+        # TODO: Doesn't catch www.
+        regex = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
+        return regex.sub(url_tag, text)
+
+
+class RedditSubmission:
+    def __init__(self, source):
+        self.source = source
+        self.branches = []
+
+    def add_annotation_branch(self, annotation_branch):
+        """Add a branch as a list of annotations to this submission"""
+        self.branches.append(annotation_branch)
+
+
+class RedditDataset:
     def __init__(self):
-        self.annotations = []
-        self.current_index = 0
+        # self.annotations = []
+        self.submissions = []
+        self.last_submission = lambda: len(self.submissions) - 1
         # mapping from property to tuple: (min, max)
         self.min_max = {
             'karma': [0, 0],
@@ -67,9 +116,27 @@ class Annotations:
         self.max_i = 1
         self.karma_max = 0
         self.karma_min = 0
-        self.freq_histogram = []
+        # dictionary at idx #num is used for label #num, example: support at 0
+        self.freq_histogram = [dict(), dict(), dict(), dict()]
+        self.sdqc_to_int = {
+            "Supporting": 0,
+            "Denying": 1,
+            "Querying": 2,
+            "Commenting": 3
+        }
 
-    def add_annotation(self, annotation):
+    def add_reddit_submission(self, source):
+        self.submissions.append(RedditSubmission(RedditAnnotation(source, is_source=True)))
+
+    def add_submission_branch(self, branch):
+        annotation_branch = []
+        for annotation in branch:
+            annotation = RedditAnnotation(annotation)
+            self.analyse_annotation(annotation)
+            annotation_branch.append(annotation)
+        self.submissions[self.last_submission()].add_annotation_branch(annotation_branch)
+
+    def analyse_annotation(self, annotation):
         if not annotation:
             return
         self.handle(self.min_max['karma'], annotation.user_karma)
@@ -81,15 +148,15 @@ class Annotations:
                         sum([len(word) for word in annotation.tokens]) / word_len)
         self.handle(self.min_max['upvotes'], annotation.upvotes)
         self.handle(self.min_max['reply_count'], annotation.reply_count)
+        self.handle_frequent_words(annotation)
 
-        self.annotations.append(annotation)
         return annotation
 
-    def handle(self, entries, property):
-        if property > entries[self.max_i]:
-            entries[self.max_i] = property
-        if property < entries[self.min_i] or entries[self.min_i] == 0:
-            entries[self.min_i] = property
+    def handle(self, entries, prop):
+        if prop > entries[self.max_i]:
+            entries[self.max_i] = prop
+        if prop < entries[self.min_i] or entries[self.min_i] == 0:
+            entries[self.min_i] = prop
 
     def get_min(self, key):
         return self.min_max[key][self.min_i]
@@ -97,53 +164,44 @@ class Annotations:
     def get_max(self, key):
         return self.min_max[key][self.max_i]
 
-    # def handle_frequent_words(self, annotation):
     # TODO: Make histogram of frequent words per class
-
-    def make_frequent_words(self, take_count=100, use_parent_sdqc=0):
+    def handle_frequent_words(self, annotation, use_parent_sdqc=False):
         # Most frequent words for annotation classes, string to int (word counter)
+        dict_idx = self.sdqc_to_int[annotation.sdqc_parent] \
+            if use_parent_sdqc else self.sdqc_to_int[annotation.sdqc_submission]
+        for token in annotation.tokens:
+            current_histo = self.freq_histogram[dict_idx]
+            if token in current_histo:
+                current_histo[token] = current_histo[token] + 1
+            else:
+                current_histo[token] = 1
 
-        # dictionary at idx #num is used for label #num, example: support at 0
-        self.freq_histogram = [dict(),dict(),dict(),dict()]
-        
-        sdqc_to_int = {
-            "Supporting": 0,
-            "Denying": 1,
-            "Querying": 2,
-            "Commenting": 3
-        }
-
-        for annotation in self.annotations:
-            dict_idx = sdqc_to_int[annotation.sdqc_parent] if use_parent_sdqc == 0 else sdqc_to_int[annotation.sdqc_submission]
-            for token in annotation.tokens:
-                current_histo = self.freq_histogram[dict_idx]
-                if token in current_histo:
-                    current_histo[token] = current_histo[token] + 1
-                else:
-                    current_histo[token] = 1
-        
+    def get_frequent_words(self, take_count=100):
+        histogram = {}
         for idx in range(len(self.freq_histogram)):
             keys = [(self.freq_histogram[idx][key],key) for key in self.freq_histogram[idx].keys()]
             keys.sort()
             keys.reverse()
 
-            self.freq_histogram[idx] = keys[:take_count]
-
+            histogram[idx] = keys[:take_count]
+        return histogram
 
     def iterate(self):
-        for annotation in self.annotations:
-            yield annotation
+        for submission in self.submissions:
+            for branch in submission.brances:
+                for annotation in branch:
+                    yield annotation
     
     #TODO: the filter methods below can also extract all urls / quotes to get count of them as features maybe?
 
-    # filters text of all annotations to replace reddit quotes with 'Reference'
-    def filter_reddit_quotes(self):
-        regex = re.compile(r"^>*\n$")
-        for annotation in self.annotations:
-            annotation.text = regex.sub("Reference", annotation.text)
-    
-    # filters text of all annotations to replace urls.
-    def filter_text_urls(self):
-        regex = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
-        for annotation in self.annotations:
-            annotation.text = regex.sub("url", annotation.text)
+    # # filters text of all annotations to replace reddit quotes with 'Reference'
+    # def filter_reddit_quotes(self):
+    #     regex = re.compile(r"^>*\n$")
+    #     for annotation in self.annotations:
+    #         annotation.text = regex.sub("REFREFREF", annotation.text)
+    #
+    # # filters text of all annotations to replace urls.
+    # def filter_text_urls(self):
+    #     regex = re.compile(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
+    #     for annotation in self.annotations:
+    #         annotation.text = regex.sub("URLURLURL", annotation.text)
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,7 +1,6 @@
 import os, json, csv, sys
 import word_embeddings
-from classes.Annotation import CommentAnnotation
-from classes.Annotation import Annotations
+from classes.Annotation import RedditDataset
 from classes.Features import FeatureExtractor
 
 datafolder = '../data/'
@@ -30,22 +29,24 @@ with open('../data/lexicon/negation_words.txt', "r") as negation_word_file:
         negation_words.append(line.strip().lower())
 
 
-def loadAnnotations(datafolder):
-    if not datafolder:
+def loadAnnotations(filename):
+    if not filename:
         return
-    annotations = Annotations()
-    for rumour_folder in os.listdir(datafolder):
-        rumour_folder_path = os.path.join(datafolder, rumour_folder)
+    dataset = RedditDataset()
+    for rumour_folder in os.listdir(filename):
+        rumour_folder_path = os.path.join(filename, rumour_folder)
         if not os.path.isdir(rumour_folder_path):
             continue
         for submission_json in os.listdir(rumour_folder_path):
-            file_json_path = os.path.join(rumour_folder_path, submission_json)
-            with open(file_json_path, "r", encoding='utf-8') as file:
-                json_list = json.load(file)
-                for json_obj in json_list:
-                    annotation = CommentAnnotation(json_obj)
-                    annotations.add_annotation(annotation)
-    return annotations
+            submission_json_path = os.path.join(rumour_folder_path, submission_json)
+            with open(submission_json_path, "r", encoding='utf-8') as file:
+                json_obj = json.load(file)
+                sub = json_obj['redditSubmission']
+                dataset.add_reddit_submission(sub)
+                branches = json_obj['branches']
+                for branch in branches:
+                    dataset.add_submission_branch(branch)
+    return dataset
 
 
 def preprocess(annotations, wembs=None, emb_dim=100):
@@ -65,36 +66,36 @@ def write_preprocessed(preprocessed_data, filename):
         for (id, sdqc_p, sdqc_s, vec) in preprocessed_data:
             csv_writer.writerow([id, sdqc_p, sdqc_s, vec])
 
-def get_branches(annotation_list):
-    # comments with no replies are bottom level
-    bottom_annotations = list(filter(lambda x: x.reply_count == 0, annotation_list))
-
-    #get branch of each bottom level comment
-    branches = [get_branch_of_bottom(x, annotation_list) for x in bottom_annotations]
-    return branches
-
-# used by 'get_branches' to get all branches from a list of annotations
-def get_branch_of_bottom(bottom_level_comment, annotation_list):
-    # return empty list if deleted
-    if bottom_level_comment.is_deleted:
-        return []
-    
-    # return list containing itself is it is top level comment
-    parent_id = bottom_level_comment.parent_id
-    if parent_id == bottom_level_comment.submission_id:
-        return [bottom_level_comment]
-    
-    branch = []
-    current_comment = bottom_level_comment
-    # continue until top level
-    while current_comment.parent_id != current_comment.submission_id:
-        branch.append(current_comment)
-        # set current comment to parent
-        current_comment = [x for x in annotation_list if x.comment_id == current_comment.parent_id][0]
-
-    # this is top level comment, add
-    branch.append(current_comment)
-    return branch
+# def get_branches(annotation_list):
+#     # comments with no replies are bottom level
+#     bottom_annotations = list(filter(lambda x: x.reply_count == 0, annotation_list))
+#
+#     #get branch of each bottom level comment
+#     branches = [get_branch_of_bottom(x, annotation_list) for x in bottom_annotations]
+#     return branches
+#
+# # used by 'get_branches' to get all branches from a list of annotations
+# def get_branch_of_bottom(bottom_level_comment, annotation_list):
+#     # return empty list if deleted
+#     if bottom_level_comment.is_deleted:
+#         return []
+#
+#     # return list containing itself is it is top level comment
+#     parent_id = bottom_level_comment.parent_id
+#     if parent_id == bottom_level_comment.submission_id:
+#         return [bottom_level_comment]
+#
+#     branch = []
+#     current_comment = bottom_level_comment
+#     # continue until top level
+#     while current_comment.parent_id != current_comment.submission_id:
+#         branch.append(current_comment)
+#         # set current comment to parent
+#         current_comment = [x for x in annotation_list if x.comment_id == current_comment.parent_id][0]
+#
+#     # this is top level comment, add
+#     branch.append(current_comment)
+#     return branch
 
 def main(argv):
     # data_folder = wembs = emb_dim = None
