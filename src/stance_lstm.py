824a33bee1bded1afb559ececca241a57f230082
anders <anders@edelbolillie.dk>
2019-03-30 12:08:42 +0100

    Setup for multiple layers

--- src/stance_lstm.py
+++ src/stance_lstm.py
@@ -4,31 +4,43 @@ import torch.nn.functional as F
 import sklearn.metrics as sk
 import numpy as np
 from sklearn.metrics import classification_report
+import collections
 
 import data_loader
 
 class StanceLSTM(nn.Module):
-    def __init__(self, emb_dim, hidden_dim, num_labels, vocab_size=0, pre_trained=True):
+    def __init__(self, lstm_layers, lstm_dim, hidden_layers, hidden_dim,
+                 num_labels, emb_dim, vocab_size=0, dropout=False, pre_trained=True):
         super(StanceLSTM, self).__init__()
         
         self.hidden_dim = hidden_dim
+        self.lstm_layers = lstm_layers
         self.pre_trained = pre_trained
         if not pre_trained:
             self.word_embeddings = nn.Embedding(vocab_size, emb_dim)
 
         # LSTM layer
-        self.lstm = nn.LSTM(emb_dim, hidden_dim, 2)
+        self.lstm = nn.LSTM(emb_dim, lstm_dim, lstm_layers)
 
         # Linear layer
-        self.hidden2label = nn.Linear(hidden_dim, num_labels)
+        dense_layers = collections.OrderedDict()
+        dense_layers["lin0"] = torch.nn.Linear(lstm_dim, hidden_dim)
+        dense_layers["rec0"] = torch.nn.ReLU()
+        for i in range(hidden_layers - 1):
+            dense_layers["lin%d" % (i + 1)] = torch.nn.Linear(hidden_dim, hidden_dim)
+            dense_layers["rec%d" % (i + 1)] = torch.nn.ReLU()
+        if dropout:
+            dense_layers["drop"] = torch.nn.Dropout(p=0.5)
+        dense_layers["lin%d" % hidden_layers] = torch.nn.Linear(hidden_dim, num_labels)
+        self.hidden2label = torch.nn.Sequential(dense_layers)
 
         #initialize state
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
         # The axes semantics are (hidden_layers, minibatch_size, hidden_dim)
-        return (torch.zeros(2, 1, self.hidden_dim),
-                torch.zeros(2, 1, self.hidden_dim))
+        return (torch.zeros(self.lstm_layers, 1, self.hidden_dim),
+                torch.zeros(self.lstm_layers, 1, self.hidden_dim))
 
     def forward(self, data):
         x = data
@@ -39,36 +51,15 @@ class StanceLSTM(nn.Module):
         label_space = self.hidden2label(lstm_out[-1])
         label_scores = F.log_softmax(label_space, dim=1)
         return label_scores
-    
 
-# EMB = 100
-# HIDDEN_DIM = 6
-# EPOCHS = 100
-
-# def prepare_sequence(seq, to_ix):
-#     idxs = [to_ix[w] for w in seq.split()]
-#     return torch.tensor(idxs, dtype=torch.long)
-
-# training_data = [
-#     ("The dog ate the apple", 'C'),
-#     ("Everybody read that book", 'D')
-# ]
-
-# w2i = {}
-# for sent, _ in training_data:
-#     for word in sent.split():
-#         if word not in w2i:
-#             w2i[word] = len(w2i)
-# w2i['cat'] = len(w2i) #For testing
-# w2i['page'] = len(w2i)#For testing
 l2i = {'S': 0, 'D': 1, 'Q': 2, 'C': 3}
 
 X_train, X_test, y_train, y_test, emb_size = data_loader.get_train_test_split()
 EMB = emb_size
 HIDDEN_DIM = 300
-EPOCHS = 20
+EPOCHS = 10
 
-model = StanceLSTM(EMB, HIDDEN_DIM, len(l2i), pre_trained=True)
+model = StanceLSTM(2, 100, 2, 100, len(l2i), EMB)
 loss_func = nn.NLLLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
@@ -83,7 +74,6 @@ for epoch in range(EPOCHS):
         model.hidden = model.init_hidden()
 
         # Prepare data
-        # sentence_in = prepare_sequence(sentence, w2i)
         data_in = torch.tensor([vec])
         target = torch.tensor([label])
 
@@ -109,17 +99,16 @@ with torch.no_grad():
         pred = model(data_in)
         predicted = torch.argmax(pred.data, dim=1)
         pred_val = predicted[0].item()
-        # print("Predicted: {0}\tActual: {1}".format(pred_val,label))
         labels_pred.append(pred_val)
     print(classification_report(labels_true, labels_pred))
-    # cm = sk.confusion_matrix(labels_true, labels_pred, labels=[0, 1, 2, 3])
-    # print("Confusion matrix:")
-    # print("  S  D  Q  C")
-    # print(cm)
-    # cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    cm = sk.confusion_matrix(labels_true, labels_pred, labels=[0, 1, 2, 3])
+    print("Confusion matrix:")
+    print("  S  D  Q  C")
+    print(cm)
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    acc = sk.accuracy_score(labels_true, labels_pred)
+    print("Accuracy: %.5f" % acc)
     # sdqc_acc = cm.diagonal()
-    # acc = sk.accuracy_score(labels_true, labels_pred)
     # f1 = sk.f1_score(labels_true, labels_pred, average='macro')
     # print("SDQC acc:", sdqc_acc)
-    # print("Accuracy: %.5f" % acc)
     # print("F1-macro:", f1)
