91067e37b8dec77f5bb6b1e4354a2442ca2a3d01
anders <anders@edelbolillie.dk>
2019-04-08 13:31:44 +0200

    Write out results to files

---
+++ output/README.md
@@ -0,0 +1,6 @@
+# Experiment output
+Contains output from running experiments on stance classification.
+
+Files ending with '_parameters.txt' include reports on hyper parameter search.
+
+Files ending with '_cm.png' include confusion matrices as a result of running classification on the evaluation/test set with the optimal parameters found for a given classifier.
--- src/stance_parameter_search.py
+++ src/stance_parameter_search.py
@@ -6,16 +6,14 @@ from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-import argparse
+import argparse, os
 import data_loader
+
+output_folder = '../output/'
 
 parser = argparse.ArgumentParser(description='Preprocessing of data files for stance classification')
 parser.add_argument('-i', '--input_file', dest='file', default='../data/preprocessed/preprocessed.csv',
                     help='Input file holding train data. If a folder, iterates files within.')
-# parser.add_argument('-a', '--acc', dest='accuracy', default=False, action='store_true',
-#                     help='Enable accuracy scoring metric')
-# parser.add_argument('-f', '--f1_macro', dest='f1_macro', default=False, action='store_true',
-#                     help='Enable F1 macro scoring metric')
 args = parser.parse_args()
 
 
@@ -94,42 +92,56 @@ scores = [
 skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
 for name, estimator, tuned_parameters in settings:
-    print("# Tuning hyper-parameters on F1 macro for %s" % name)
-    print()
-    clf = GridSearchCV(
-        estimator, tuned_parameters, scoring=scores, n_jobs=-1, error_score=0, refit='f1_macro',
-        cv=skf
-    )
-    clf.fit(X_train, y_train)
-
-    print("Best parameters set found on development set for F1 macro:")
-    print()
-    print("%0.3f for %r" % (clf.best_score_, clf.best_params_))
-    print()
-    print("Grid scores on development set:")
-    print()
-    results = clf.cv_results_
-    for scorer in scores:
-        print(scorer)
-        means = results['mean_test_%s' % scorer]
-        stds = results['std_test_%s' % scorer]
-        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-            print("%0.3f (+/-%0.03f) for %r"
-                  % (mean, std * 2, params))
+    filepath = os.path.join(output_folder, name)
+    with open('%s_parameters.txt' % filepath, 'w+') as outfile:
+        s = "# Tuning hyper-parameters on F1 macro for %s" % name
+        print(s)
+        outfile.write(s + '\n')
         print()
+        clf = GridSearchCV(
+            estimator, tuned_parameters, scoring=scores, n_jobs=-1, error_score=0, refit='f1_macro',
+            cv=skf
+        )
+        clf.fit(X_train, y_train)
 
-    print("Detailed classification report:")
-    print()
-    print("The model is trained on the full development set.")
-    print("The scores are computed on the full evaluation set.")
-    print()
-    y_true, y_pred = y_test, clf.predict(X_test)
+        s = "Best parameters set found on development set for F1 macro:"
+        print(s)
+        outfile.write(s + '\n')
+        print()
+        s = "%0.3f for %r" % (clf.best_score_, clf.best_params_)
+        print(s)
+        outfile.write(s + '\n')
+        print()
+        s = "Grid scores on development set:"
+        print(s)
+        outfile.write(s + '\n')
+        print()
+        results = clf.cv_results_
+        for scorer in scores:
+            print(scorer)
+            outfile.write(scorer + '\n')
+            means = results['mean_test_%s' % scorer]
+            stds = results['std_test_%s' % scorer]
+            for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+                s = "%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params)
+                print(s)
+                outfile.write(s + '\n')
+            print()
 
-    print(classification_report(y_true, y_pred))
-    print()
-    # np.set_printoptions(precision=2)
-    plot_confusion_matrix(y_test, y_pred, title='Confusion matrix, without normalization')
-    # plt.show()
+        outfile.write('Classification report for results on evaluation set:' + '\n')
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(X_test)
+        s = classification_report(y_true, y_pred)
+        print(s)
+        outfile.write(s + '\n')
+        print()
+        np.set_printoptions(precision=2)
+        plot_confusion_matrix(y_test, y_pred, title='Confusion matrix, without normalization')
+        plt.savefig('%s_cm.png' % filepath, bbox_inches='tight')
 
 
 
