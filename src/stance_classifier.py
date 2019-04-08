80e484b2cb406522045373ec285b63d32e7c0953
anders <anders@edelbolillie.dk>
2019-04-08 14:08:07 +0200

    Add cmd arguments and output

--- src/stance_classifier.py
+++ src/stance_classifier.py
@@ -5,8 +5,21 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.dummy import DummyClassifier
-
+import argparse, os
 import data_loader
+
+output_folder = '../output/'
+
+parser = argparse.ArgumentParser(description='Preprocessing of data files for stance classification')
+parser.add_argument('-i', '--input_file', dest='file', default='../data/preprocessed/preprocessed.csv',
+                    help='Input file holding train data. If a folder, iterates files within.')
+parser.add_argument('-k', '--k_folds', dest='k_folds', const=5, type=int, nargs='?',
+                    help='Number of folds for cross validation (default=5)')
+parser.add_argument('-a', '--accuracy', dest='acc', action='store_true', default=False,
+                    help='Enable accuracy metric')
+parser.add_argument('-f', '--f1_macro', dest='f1_macro', action='store_true', default=False,
+                    help='Enable F1 macro metric')
+args = parser.parse_args()
 
 classifiers = {
     'Logistic Regression': LogisticRegression(solver='liblinear', C=10, penalty='l1', multi_class='auto'),
@@ -17,16 +30,23 @@ classifiers = {
     'Stratified Random': DummyClassifier(strategy='stratified')
 }
 
-scoring = [
-    'accuracy',
-    'f1_macro'
-]
+X, y, _ = data_loader.get_features_and_labels(filename=args.file)
+skf = StratifiedKFold(n_splits=args.k_folds, shuffle=True, random_state=42)
 
-X, y, _ = data_loader.get_features_and_labels()
-skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=42)
-for score in scoring:
-    print()
-    for name, clf in classifiers.items():
-        scores = cross_val_score(clf, X, y, cv=skf, scoring=score)
-        print("%-20s%s %0.2f (+/- %0.2f)" % (name, score, scores.mean(), scores.std() * 2))
+
+def cross_val(score):
+    filepath = os.path.join(output_folder, 'cross_val_scoring')
+    with open('%s_%s.txt' % (filepath, score), 'w+') as outfile:
+        for name, clf in classifiers.items():
+            scores = cross_val_score(clf, X, y, cv=skf, scoring=score)
+            s = "%-20s%s %0.2f (+/- %0.2f)" % (name, score, scores.mean(), scores.std() * 2)
+            print(s)
+            outfile.write(s + '\n')
+
+
+if args.acc:
+    cross_val('accuracy')
+if args.f1_macro:
+    cross_val('f1_macro')
+
 
