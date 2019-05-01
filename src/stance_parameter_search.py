4c6748cb0ebec141738790adc7bb7d3297f7ecc7
anders <anders@edelbolillie.dk>
2019-05-01 16:02:42 +0200

    Fix random parameter distributions and add arguments

--- src/stance_parameter_search.py
+++ src/stance_parameter_search.py
@@ -22,6 +22,10 @@ parser.add_argument('-y', '--test_file', dest='test_file', default='../data/prep
                     help='Input file holding test data')
 parser.add_argument('-k', '--k_folds', dest='k_folds', default=3, type=int, nargs='?',
                     help='Number of folds for cross validation (default=5)')
+parser.add_argument('-g', '--grid', default=False, action='store_true',
+                    help='Enable GridSearchCV, otherwise use RandomizedSearchCV')
+parser.add_argument('-n', '--rand_samples', default=10, type=int, nargs='?',
+                    help='Number of random samples if using RandomizedSearchCV')
 args = parser.parse_args()
 
 X_train, X_test, y_train, y_test, _, feature_mapping = data_loader.load_train_test_data(
@@ -40,17 +44,17 @@ settings = [
 ]
 
 settings_rand = [
-    ('rbf-svm', SVC(), {'kernel': ['rbf'], 'gamma': sp_expon(scale=.1), 'C': sp_expon(scale=100),
+    ('rbf-svm', SVC(), {'kernel': ['rbf'], 'gamma': sp_expon(scale=.1), 'C': sp_randint(1, 1000),
                         'class_weight': ['balanced', None]}),
-    # ('linear-svm', SVC(), {'kernel': ['linear'], 'C': sp_expon(scale=100), 'class_weight': ['balanced', None]}),
+    # ('linear-svm', SVC(), {'kernel': ['linear'], 'C': sp_randint(1, 1000), 'class_weight': ['balanced', None]}),
     # ('tree', DecisionTreeClassifier(), {'criterion': ['entropy', 'gini'], 'splitter':['best', 'random'],
     #                                     'max_depth': [3, None], "min_samples_split": sp_randint(2, 11),
     #                                     'max_features': ['auto', 'log2', None], 'class_weight': ['balanced', None],
     #                                     'presort': [True]}),
     # ('logistic-regression', LogisticRegression(), {'solver': ['liblinear', 'saga'], 'penalty':['l1', 'l2'],
     #                                                'class_weight': ['balanced', None],
-    #                                                'C': sp_expon(scale=100), 'multi_class': ['auto']}),
-    # ('random-forest', RandomForestClassifier(), {'n_estimators': [10, 100, 1000], 'criterion': ['entropy', 'gini'],
+    #                                                'C': sp_randint(1, 1000), 'multi_class': ['auto']}),
+    # ('random-forest', RandomForestClassifier(), {'n_estimators': sp_randint(10, 2000), 'criterion': ['entropy', 'gini'],
     #                                              'max_depth': [3, None], 'max_features': ['auto', 'log2', None],
     #                                              "min_samples_split": sp_randint(2, 11), "bootstrap": [True, False],
     #                                              'class_weight': ['balanced_subsample', None]})
@@ -62,10 +66,10 @@ skf = StratifiedKFold(n_splits=args.k_folds, shuffle=True, random_state=42)
 features = data_loader.get_features()
 feature_names = features.keys()
 
-rand_search = True
-rand_iter = 50
+grid_search = args.grid  # whether to use GridSearchCV or RandomizedSearchCV
+rand_iter = args.rand_samples  # number of random samples to use
 
-for name, estimator, tuned_parameters in (settings_rand if rand_search else settings):
+for name, estimator, tuned_parameters in (settings_rand if not grid_search else settings):
     start = time.time()
     filepath = os.path.join(output_folder, name)
     if not os.path.exists(filepath):
@@ -82,7 +86,7 @@ for name, estimator, tuned_parameters in (settings_rand if rand_search else sett
             X_train_ = data_loader.select_features(X_train, feature_mapping, features)
             X_test_ = data_loader.select_features(X_test, feature_mapping, features)
             with open('%s/parameters_%s.txt' % (filepath, feature_name), 'w+') as outfile:
-                if rand_search:
+                if not grid_search:
                     clf = RandomizedSearchCV(
                         estimator, tuned_parameters, scoring=scorer, n_jobs=--1, error_score=0, n_iter=rand_iter,
                         cv=skf, iid=False, return_train_score=False, pre_dispatch=None
